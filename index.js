51b14a861bc5f1bbeb050e8f8db897a832a18efb
Steve Pieper <pieper@bwh.harvard.edu>
2014-07-15 12:30:57 -0400

    Some more bones and a bit of flesh

---
+++ index.js
@@ -0,0 +1,15 @@
+//
+// Copyright 2014 Isomics, Inc.
+// Author: Steve Pieper
+//
+// Distributed under the terms of the Slicer Licesnse v 1.0
+// (see License.txt in this distribution).
+//
+
+//
+// This is the main entry point for the ch package
+//
+var paths = require('./lib/paths.js')
+  ;
+
+exports.paths = paths;
